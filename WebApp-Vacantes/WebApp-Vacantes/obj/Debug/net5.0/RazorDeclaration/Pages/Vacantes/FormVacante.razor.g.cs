3230d54e68558b137f3a73a9e2d2bf5d3f24b9b0
rafael <rafaelemilioabreuvaldez@gmail.com>
2021-06-13 21:45:48 -0400

    alertas añadidas

--- WebApp-Vacantes/WebApp-Vacantes/obj/Debug/net5.0/RazorDeclaration/Pages/Vacantes/FormVacante.razor.g.cs
+++ WebApp-Vacantes/WebApp-Vacantes/obj/Debug/net5.0/RazorDeclaration/Pages/Vacantes/FormVacante.razor.g.cs
@@ -13,126 +13,126 @@ namespace WebApp_Vacantes.Pages.Vacantes
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 1 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 2 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 3 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 4 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 5 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 6 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 7 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 8 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using WebApp_Vacantes;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 9 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using WebApp_Vacantes.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 10 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using apiVacantes.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 11 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 12 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using Microsoft.AspNetCore.Http.Extensions;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 13 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using Microsoft.AspNetCore.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 14 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
+#line 15 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\_Imports.razor"
 using Radzen.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 1 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\Pages\Vacantes\FormVacante.razor"
+#line 1 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\Pages\Vacantes\FormVacante.razor"
 using System.IO;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\Pages\Vacantes\FormVacante.razor"
+#line 2 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\Pages\Vacantes\FormVacante.razor"
 using Microsoft.AspNetCore.Hosting;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\Pages\Vacantes\FormVacante.razor"
+#line 5 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\Pages\Vacantes\FormVacante.razor"
            [Authorize(Roles = "admin, postulante")]
 
 #line default
@@ -146,7 +146,7 @@ using Microsoft.AspNetCore.Hosting;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 127 "C:\Users\lisan\OneDrive\Escritorio\TAREAS C5\Prog 3\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\Pages\Vacantes\FormVacante.razor"
+#line 127 "E:\prog3\Nueva carpeta\BolsaDeEmpleos\WebApp-Vacantes\WebApp-Vacantes\Pages\Vacantes\FormVacante.razor"
        
 
     [Parameter] public Trabajos Trabajos { get; set; } = new Trabajos();
